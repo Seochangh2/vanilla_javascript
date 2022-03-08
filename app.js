1c37ea3cdaf9820e08c5c93ffa2171263b31c455
Seochangh2 <nomm89@naver.com>
2022-03-09 02:32:02 +0900

    toggle 함수 중요성

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ function titleClick() {
 }
 function mouseEnter() {
   ch2.innerText = "Hi";
-  const cName = ch2.className;
-  let newCName;
+  const cName = ch2.classList;
+  // let newCName;
   const enterClass = "enter";
-  if (cName === "") {
-    newCName = enterClass;
-  } else {
-    newCName = "";
-  }
-  ch2.className = newCName;
+  // if (cName.contains(enterClass)) {
+  //   cName.remove(enterClass);
+  // } else {
+  //   cName.add(enterClass);
+  // } 주석처리 줄이 바로 아래의 토글함수로 교체 가능
+  cName.toggle("enter");
 }
 function mouseLeave() {
   ch2.innerText = "Bye";
