a428a1ba9ca9aebcdc52a0f4e702b2531fac9971
Seochangh2 <nomm89@naver.com>
2022-03-09 02:04:04 +0900

    CSS in JavaScript

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 const ch = document.querySelector(".ch_className");
 const ch2 = document.querySelector("#ch_id");
+const wifi = document.querySelector("#wifi_id");
 
-function handleTitleClick() {
+function titleClick() {
   alert("Click!");
 }
-function handleMouseEnter() {
+function mouseEnter() {
   ch2.innerText = "Hi";
-  ch2.style.color = "blue";
+  const currentColor = ch2.style.color;
+  let newColor;
+  if (currentColor === "black") {
+    newColor = "blue";
+  } else {
+    newColor = "black";
+  }
+  ch2.style.color = newColor;
 }
-function handleMouseLeave() {
+function mouseLeave() {
   ch2.innerText = "Bye";
-  ch2.style.color = "black";
 }
-ch.addEventListener("click", handleTitleClick);
-ch.addEventListener("mouseenter", handleMouseEnter);
-ch.addEventListener("mouseleave", handleMouseLeave);
+function windowResize() {
+  document.body.style.backgroundColor = "pink";
+}
+function windowCopy() {
+  alert("Copy!");
+}
+function wifiOnline() {
+  wifi.innerText = "OnLine !";
+}
+function wifiOffline() {
+  wifi.innerText = "OFFLine !";
+}
+ch.addEventListener("click", titleClick);
+ch.addEventListener("mouseenter", mouseEnter);
+ch.addEventListener("mouseleave", mouseLeave);
+
+window.addEventListener("resize", windowResize);
+window.addEventListener("copy", windowCopy);
+window.addEventListener("offline", wifiOffline);
+window.addEventListener("online", wifiOnline);
