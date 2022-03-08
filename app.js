c14e600aa590455c45abde34e1d4af35a6be08fe
Seochangh2 <nomm89@naver.com>
2022-03-09 01:40:31 +0900

    JS 리스너 예제

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const ch = document.querySelector(".ch_className");
 const ch2 = document.querySelector("#ch_id");
 
-ch2.style.color = "red";
-
 function handleTitleClick() {
   alert("Click!");
 }
+function handleMouseEnter() {
+  ch2.innerText = "Hi";
+  ch2.style.color = "blue";
+}
+function handleMouseLeave() {
+  ch2.innerText = "Bye";
+  ch2.style.color = "black";
+}
 ch.addEventListener("click", handleTitleClick);
+ch.addEventListener("mouseenter", handleMouseEnter);
+ch.addEventListener("mouseleave", handleMouseLeave);
