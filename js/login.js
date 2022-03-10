5a219f3c62b1279acf9821d6b0e5705e8080af78
Seochangh2 <nomm89@naver.com>
2022-03-11 00:57:46 +0900

    파일 정리 및 setInterval 함수

--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 const nameH1 = document.querySelector("#name");
 
 const USERNAME_KEY = "username";
@@ -11,12 +10,11 @@ function loginSubmit(event) {
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASS);
-
   writeGreeting(username);
 }
 function writeGreeting(username) {
   nameH1.classList.remove(HIDDEN_CLASS);
-  nameH1.innerText = `Hello ${username}`;
+  nameH1.innerText = `Hello ${username} `;
 }
 
 const savedName = localStorage.getItem(USERNAME_KEY);
