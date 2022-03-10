7f34d5ec02f25ee96861cb5e2e128729710652a5
Seochangh2 <nomm89@naver.com>
2022-03-11 00:18:11 +0900

    이름 입력후 로그인시 입력창 사라짐

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
-
+const nameH1 = document.querySelector("#name");
 function loginSubmit(event) {
   event.preventDefault();
-  const value = loginInput.value;
+  const username = loginInput.value;
+  nameH1.innerText = `Hello ${username}`;
+  loginForm.classList.add("hidden");
+  nameH1.classList.toggle("hidden");
 }
 
 loginForm.addEventListener("submit", loginSubmit);
