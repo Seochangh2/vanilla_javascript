173d7256b4f8d24b809b0d22eb11002e1f8c4fb3
Seochangh2 <nomm89@naver.com>
2022-03-11 00:43:06 +0900

    로컬스토리지 활용

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 const nameH1 = document.querySelector("#name");
+
+const USERNAME_KEY = "username";
+const HIDDEN_CLASS = "hidden";
+
 function loginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASS);
+
+  writeGreeting(username);
+}
+function writeGreeting(username) {
+  nameH1.classList.remove(HIDDEN_CLASS);
   nameH1.innerText = `Hello ${username}`;
-  loginForm.classList.add("hidden");
-  nameH1.classList.toggle("hidden");
 }
 
-loginForm.addEventListener("submit", loginSubmit);
+const savedName = localStorage.getItem(USERNAME_KEY);
+
+if (savedName === null) {
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", loginSubmit);
+} else {
+  writeGreeting(savedName);
+}
