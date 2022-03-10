68513a0103c0d15b0823e80c8ef8490414f6047d
Seochangh2 <nomm89@naver.com>
2022-03-10 22:33:02 +0900

    JS로 작성했던 로그인을 html로 변환 , preventDefault 의 중요성
    html 에서 자동적으로 입력한 값을 받아오는 기능이 있음

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function loginBtnClick() {
-  console.log(loginInput.value);
-  console.log("click !");
+function loginSubmit(event) {
+  event.preventDefault();
+  const value = loginInput.value;
 }
 
-loginButton.addEventListener("click", loginBtnClick);
+loginForm.addEventListener("submit", loginSubmit);
