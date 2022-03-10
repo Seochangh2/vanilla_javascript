6ace996b28ce9137a3869c814ca08ec161cfab03
Seochangh2 <nomm89@naver.com>
2022-03-11 01:10:52 +0900

    padstart 추가

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,9 @@ const clock = document.querySelector("#clock");
 
 function setClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()} : ${date.getMinutes()}`;
+  const hours = String(date.getHours());
+  const minutes = String(date.getMinutes());
+  clock.innerText = `${hours.padStart(2, "0")} : ${minutes.padStart(2, "0")}`;
 }
 
 setInterval(setClock(), 1000);
